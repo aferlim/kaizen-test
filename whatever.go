0da6f52f589635264626fcbba47679a1787a92a9
Andre Ferreira Lima <andre.lima@grupoltm.com.br>
2019-03-29 10:43:48 -0300

    test

---
+++ whatever.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//const a, b, c int = 1, 2, 3
+
+	var a = 1
+
+	var b int16
+
+	b = 2
+
+	c := 3
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+
+	g, h, i := 2, false, "mah oi!"
+
+	fmt.Println(g, h, i)
+}
